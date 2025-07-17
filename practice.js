f15750e1c3f4e1cef3b86598d698287a2ce7814a
Lankesh8521 <lankeshk695@gmail.com>
2025-07-17 11:56:17 +0530

    dublicatestr

--- practice.js
+++ practice.js
@@ -119,19 +119,117 @@
 //             Write a function to find target in that array and return its index.
 //             It is guranteed that target will always exists in arr.
 // Time Limit: 1 sec
-// 1 <= n <= 10^6
+// 1 <= n <= 10^9
 
 // Test Case 1: arr: [2,3,7,15,46]   target: 15
 // Output:      3
 
-function indexnuber(arr,target){
-  let target = 15;
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] == target){
-      target = arr[i];
+
+
+// function indexnumber(arr,target){
+//   for(let i = 0; i < arr.length; i++){
+//     if(arr[i] == target){
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+// let arr = [2,3,7,15,46];
+// let target = 15;
+// console.log(indexnumber(arr,target));
+
+
+// function indexnumber(arr,target){
+//   let low = 0;
+//   let high = arr.length-1;
+//   while(low <= high){
+//          let mid = Math.floor((low+high)/2);
+//          if(arr[mid] == target) {
+//           return mid;
+//          }
+//          else if(arr[mid] < target){
+//           low = mid + 1;
+//          }else if(arr[mid] > target){
+//           high = mid - 1;
+//          }
+//   }
+//   return -1;
+// }
+// let arr = [3,5,7,9,15,45];
+// let target = 9;
+// console.log(indexnumber(arr,target));
+
+
+
+
+// write a function to find sum fo digit number ex-123
+//output = 6
+
+// function sumofdigit(num){
+//   let sum = 0;
+//   while(num > 0){
+//      sum += num % 10;
+//      num = Math.floor(num/10);
+//   }
+//   return sum;
+// }
+// let num = 1234;
+// console.log(sumofdigit(num));
+
+
+
+//write a function to remove dublicate from string ex-akshay,
+//output - akshy
+
+function dublicatename(str){
+    let ans = "";
+    let hash = {
+
     }
-  }
-  return target;
+   for(let i = 0; i< str.length; i++){
+    if(hash[str[i]] != true){
+        ans += str[i];
+        hash[str[i]] = true; 
+
+    }
+
+   }  
+   return ans;  
 }
-let arr = [2,3,7,15,46];
-console.log(indexnuber(arr));
+let str = "akshay";
+console.log(dublicatename(str));
+
+
+// write a function to find second largest number of array
+
+
+// function SecondLargest(arr) {
+//   let first = 0;
+//   let second = 0;
+//  for (let i = 0; i < arr.length; i++) {
+//      let num = arr[i];
+//     if (num > first) {
+//       second = first;
+//       first = num;
+//     } else if (num > second && num < first) {
+//       second = num;
+//     }
+//   }
+//   return second;
+// }
+// let arr = [1,2,1,5,7,9,8,11];
+// console.log(SecondLargest(arr));
+
+
+
+//write a function to cheak if a number is power of 3 return true if satsifiy the question else false
+
+// function ispower(num){
+//   if(num<1) return false;
+//   while(num % 3 == 0){
+//     num = num/3;
+//   }
+//   return num == 1;
+// }
+// let num = 81;
+// console.log(ispower(num));
