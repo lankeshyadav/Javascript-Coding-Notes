e525ad17ab53159d3f15338ab07847ce695341a7
Lankesh8521 <lankeshk695@gmail.com>
2025-07-23 17:00:00 +0530

    array

--- w3schoolsobject.js
+++ w3schoolsobject.js
@@ -130,15 +130,160 @@
 // console.log(res);
 
 
-const fruits = {Banana: 300, Orange: 200, Apples: 500};
-const keys = Object.keys(fruits);
-for(let i = 0; i < keys.length; i++){
-    let fruit = keys[i];
-    console.log(fruit + ": " + fruits[fruit]);
+// const fruits = {Banana: 300, Orange: 200, Apples: 500};
+// const keys = Object.keys(fruits);
+// for(let i = 0; i < keys.length; i++){
+//     let fruit = keys[i];
+//     console.log(fruit + ": " + fruits[fruit]);
+// }
+
+
+// function person(first, last, age, eye){
+//     this.firstName = first;
+//     this.latName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+// }
+// const myName = new person ("john", "singh", 23, "Black");
+// console.log(myName.firstName);
+
+// function person(first, last, age, eye){
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+// };
+// const myName = new person ("john", "singh", 23, "Black");
+// const myUser = new person ("lankesh", "kumar", 23, "White");
+// console.log(myName.lastName);
+// console.log(myUser);
+
+// function Person(first, last, age, eye) {
+//   this.firstName = first;
+//   this.lastName = last;
+//   this.age = age;
+//   this.eyeColor = eye;
+//   this.fullName = function() {
+//     return this.firstName + " " + this.lastName
+//   };
+// }
+// const myName = new Person("John", "Doe", 50, "blue");
+// console.log(myName.fullName());
+
+
+//javascript object
+
+// const cars = ["BMW",
+//     "Tata",
+//     "Valvo"
+// ];
+// console.log(cars[0]);
+
+// const cars = [];
+// cars[0]= "Tata";
+// cars[1]= "BMW";
+// cars[2]= "Volvo";
+// console.log(cars);
+
+// const cars = ["Bmw", "Aulto", "nexon"];
+// cars[0] = "Scrpio";
+// console.log(cars);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.toString());
+
+// const person = ["john", "singh", 46];
+// console.log(person[1]);
+
+
+// const person = {firstName:"john", lastName:"singh", age:23};
+// console.log(person.age);
+
+
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let length = fruits.length;
+// console.log(fruits.length);
+
+
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//let fruit = fruits[fruits.length-1];
+// console.log(fruits[fruits.length-1]);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let flen = fruits.length;
+// let text = " ";
+// for(let i = 0; i < flen; i++){
+//    text += fruits[i];
+// }
+// console.log(text);
+
+
+//douts this code
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let text = " ";
+// fruits.forEach(myFunction);
+//     text += " ";
+//     function myFunction(value){
+//         text += value;
+//     }
+// console.log(text);
+
+
+// const fruits = ["Banana", "Apple", "Mango", "Orange"];
+// fruits.push("Lemon");
+// console.log(fruits);
+
+// const fruits = ["Banana", "Orange", "Apple"];
+// fruits[fruits.length] = "Lemon";
+// console.log(fruits);
+
+
+// const fruits = ["Banana", "Orange", "Apple"];
+// fruits[6] = "Lemon";
+// console.log(fruits);
+
+
+// const person = [];
+// person[0] = "john";
+// person[1] = "kumar";
+// person[2] = 23;
+// person[3] = "india";
+// person.length;
+// console.log(person[3] + " " + person.length);
+
+
+
+// const fruits = ["Banana", "Orange", "Mango"];
+// // let type = typeof fruits;
+// console.log(typeof[fruits]);
+
+
+// const fruits = ["Banana", "Orange", "Mango"];
+// console.log(Array.isArray(fruits));
+
+
+// const fruits = ["Mango", "Orange", "Apple"];
+// console.log(fruits instanceof Array);
+
+
+
+let x = " ";
+const myObj = {
+    name: "John",
+    age:30,
+    cars: [
+        {name:"Ford", models:["Fiesta", "Focus", "Musting"]},
+        {name:"BMW", models:["320", "X3", "X5"]},
+        {name:"Fiat", models:["500", "Panda"]}
+    ]
 }
-
-
-
-
-
+for(let i in myObj.cars){
+    x += myObj.cars[i].name;
+    for(let j in myObj.cars[i].models){
+        x += myObj.cars[i].models[j];
+    }
+}
+console.log(x);
 
